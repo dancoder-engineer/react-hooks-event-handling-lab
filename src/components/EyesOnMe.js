57845b1fb63d5764e6ab42c191a062229a7acce5
Dan Seminara <japaneseforgaming@gmail.com>
2021-10-01 12:35:19 -0400

    commit tree c235f2bffd08635e083773ca8582bdd9e97004bb

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+
+function buttonBlurred(){
+    console.log('Hey! Eyes on me!')
+}
+
+function buttonFocused(){
+    console.log('Good!')
+}
+
+function EyesOnMe(props){
+    return(
+        <button onFocus={buttonFocused} onBlur={buttonBlurred}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
